83dfe09371e663176fe2b23f629db27dbad2540e
Afusat Siyanbola <afusatsiyanbola@afusats-mbp.mynetworksettings.com>
2025-05-11 17:42:44 -0400

    first commit

---
+++ bobo/clownn/okf.js
@@ -0,0 +1,324 @@
+// alert("my teacher is over 10000 years old older then ancient rome itself");
+
+// let number8 = 18;
+// let number22 = 44;
+// number8 = 62;
+
+// alert(number8 + number22);
+
+// const vilnumber = 1;
+// const vilnumber4 = 33;
+// alert(vilnumber + vilnumber4);
+
+// const firstnumber = 28;
+// const secondnumber = 17;
+
+// const sub = firstnumber - secondnumber;
+// const compare = 4 === 4;
+// alert(compare);
+// if(){}
+// const whatisyourname = prompt("what is your name");
+// if (whatisyourname === "Baseet") {
+//   alert("hello " + whatisyourname);
+// }
+// const numm = 11;
+// const numm2 = 21;
+// const add = numm + numm2;
+// const sub = numm - numm2;
+// const multi = numm * numm2;
+// const tot = add + sub > 50;
+// alert(add);
+// alert(sub);
+// alert(multi);
+// alert(tot);
+
+// const ogchoc = 150;
+// const bros = 5;
+// const take = 21;
+// const nowchoc = take * bros;
+// let newchoc = ogchoc - nowchoc;
+// alert(nowchoc);
+// alert(newchoc);
+// if (newchoc < 50) {
+//   newchoc = newchoc + 50;
+// }
+// alert("nowchocolate " + nowchoc);
+// alert("newchocolate " + newchoc);
+
+// let inp = prompt("What year is it");
+// if (inp != 2025) {
+//   alert("You are prolly in the present");
+// }
+// if (inp < 2025) {
+//   alert("You are in the past");
+// }
+// if (inp > 2025) {
+//   alert("You are in the future");
+// }
+// if ("2025" != 2025) {
+//   alert("true");
+// }
+
+// 4 === 4 true
+// "5" == 5 true
+// 2024 = 2025 none
+// "2024" != 2025 true
+// "2024" != 2024  false
+// "2036" !== 2036 true
+// 20 !== "20"  true
+// 4 === 7 false
+// let pom = 15;
+
+// if (pom <= 5) console.log("Whatsup");
+// else if (pom <= 16);
+// console.log ("whats down")
+
+// else  ( pom < 5 );
+// console.log ("whats bright")
+
+// const whatsagem = prompt("what is your age");
+// if (whatsagem > 18) {
+//   alert("Hello welcome to the country full aged citzen");
+// } else if (whatsagem < 18) {
+//   alert("Not born try again in a couple years");
+// } else {
+//   console.log("you need to answer more questions to find your age");
+//   alert("what year were you born in");
+//   alert("where were you born at");
+// }
+
+// const whenig = prompt("What year was instagram created");
+// if (whenig < 2010) {
+//   alert("wow you are early");
+// } else if (whenig > 2010) {
+//   alert("you are LATE");
+// } else {
+//   alert("you are correct");
+//   alert("well just maybe correct");
+// }
+
+// const ageoo = prompt("What is your age");
+// if (ageoo <= 19 || ageoo >= 13) {
+//   alert("hey welcome to they teenager club");
+// }
+
+// const pro = prompt(" What is your Login");
+
+// if (pro == "Admin") {
+//   const pas = prompt("What is your Passaword");
+
+//   if (pas == "TheMaster") {
+//     alert("Welcome");
+//   } else if (pas == "" || pas == null) {
+//     alert("Cancel");
+//   } else {
+//     alert("Wrong Password");
+//   }
+// } else if (pro == "" || pro == null) {
+//   alert("Canceled");
+// } else {
+//   alert("I have no idea");
+// }
+// const gra = prompt("What is your grade average");
+// if (gra >= 90) {
+//   alert("Goood A student");
+// } else if (gra >= 80 && gra <= 89) {
+//   alert("Good B Student");
+// } else if (gra >= 70 && gra <= 79) {
+//   alert("Decent C Student");
+// } else if (gra >= 65 && gra <= 69) {
+//   alert("Wow go to tutor");
+// } else {
+//   alert("You are Failing");
+// }
+// const numml = prompt("Is the number postive , negative , or zero");
+// if (numml > 0) {
+//   alert("You are a Positive Number");
+// } else if (numml < 0) {
+//   alert("You are a negative Number");
+// } else if (numml == 0) {
+//   alert("You are equal to zero");
+// } else {
+//   alert("I have no clue who you are");
+// }
+
+// const li = 40;
+// const ji = -10;
+// const vi = 2;
+
+// if (li > ji && li > vi) {
+//   alert("Li is the greatest number");
+// } else if (ji > li && ji > vi) {
+//   alert("ji is the greatest number");
+// } else if (vi > ji && vi > li) {
+//   alert("vi is the gretaest number");
+// }
+
+// const eve = prompt("Give me a number");
+// if (eve % 2 == 0) {
+//   alert(true);
+//
+
+// const num1 = prompt("Enter number 1");
+// const num2 = prompt("Enter number 2");
+// const convertednum1 = parseInt(num1);
+// const convertednum2 = parseInt(num2);
+// if (isNaN(convertednum1) || isNaN(convertednum2)) {
+//   alert("please input a valid number");
+
+//   return;
+// }
+
+// // switch (convertednum1 - convertednum2) {
+// //   case 10:
+// //     alert("hey this is 10");
+// //     break;
+// //   case 5:
+// //     alert("hey this is 10 from case 2");
+// //     break;
+
+// //   case 2:
+// //     alert("hey this is 10");
+// //     break;
+
+// //   case 7:
+// //     alert("hey this is 10");
+// //     break;
+
+// //   case 500:
+// //     alert("hey this is 10");
+// //     break;
+
+// //   case 223:
+// //     alert("hey this is 10");
+// //     break;
+
+// //   case 110:
+// //     alert("hey this is 10");
+// //     break;
+
+// //   case 1110:
+// //     alert("hey this is 10");
+// //     break;
+// //   default:
+// //     alert("BYE BYE");
+// //     break;
+// // }
+// const age = prompt("How old are you");
+
+// if (18 <= age) {
+//   alert("You are the age to votes");
+// } else if (18 > age) {
+//   alert("You are not the age to vote");
+// }
+
+// const la = prompt("enter number");
+// const sm = prompt("enter number");
+// const consml = parseInt(sm);
+// const conla = parseInt(la);
+// if (conla > consml) {
+//   alert("conla is the greatest number");
+// } else if (consml > conla) {
+//   alert("sm is the gretatest number ");
+// } else if (consml == conla) {
+//   alert("sm equals to la");
+// }
+
+// const chilv = prompt("what is the age level");
+// if (chilv <= 12) {
+//   alert("you are a child");
+// } else if (chilv >= 13 && chilv <= 19) {
+//   alert("You are a teen");
+// } else if (chilv >= 20) {
+//   alert("You are grown");
+// }
+
+// const sign = prompt("If you are negative, positive or 0 ");
+// if (sign == 0) {
+//   alert("you are 0");
+// } else if (sign > 0) {
+//   alert("you are a positive number");
+// } else if (sign < 0) {
+//   alert("you are a negative number");
+// }
+
+// const gra = prompt("what is your grade");
+// if (gra >= 90) {
+//   alert(" A student");
+// } else if (gra >= 80 && gra <= 89) {
+//   alert(" B Student");
+// } else if (gra >= 70 && gra <= 79) {
+//   alert(" C Student");
+// } else if (gra >= 65 && gra <= 69) {
+//   alert("You are a D student");
+// } else {
+//   alert("you are probably failing");
+// }
+// ````;
+// const div = prompt("is 5 and 3 divisible number");
+// if (5 % 2 == 1) {
+//   alert("false 5 is not a divisble number");
+// } else if (3 % 2 == 1) {
+//   alert("false  is not a divisble number");
+// }
+
+// let remain = prompt(" Are you odd or Even number");
+// let remainder = remain % 2;
+// if (remainder == 0) {
+//   alert("you are a even number");
+// } else if (remainder > 0) {
+//   alert("You are a odd number");
+// } else {
+//   alert("No clue who you are");
+// }
+
+// let livv = 50;
+// livv = 2;
+// alert(livv);
+
+// let cry = 2;
+
+// let sub = cry + livv;
+// alert(sub);
+
+// let fine = 15;
+// let fines = 8;
+// let truesub = fine - fines;
+// alert(truesub);
+
+// let gone = 45 / 5;
+// alert(gone);
+
+// bye = 4 * 20;
+// alert(bye);
+
+// let zik = 100 % 8;
+// alert(zik);
+
+// let num1 = prompt("enter a number");
+// let num2 = prompt("enter a number");
+// if (num1 > num2) {
+//   alert("yea num1 is greater then num2");
+// } else if (num1 < num2) {
+//   alert("num2 is greater then num 1");
+// }
+let compt = prompt("What year was this fine company established");
+if (compt >= 1990 && compt <= 2000) {
+  alert("You and your company is kindly welcome here");
+} else {
+  alert("get out of here you will never be welcome");
+}
+
+let numy1 = prompt("Choose any number");
+let numy2 = prompt("Choose any number");
+if (numy1 > numy2) {
+} else if (numy1 % numy2 == 0) {
+} else if (numy2 > numy1) {
+}
+
+if (numy1 > numy2) {
+  alert("that is true");
+} else if (numy2 > numy1) {
+  alert("That is false");
+} else if (1 % 2) {
+}
